d413088d9921580390450350115a7ee72fed8d71
Jaco Kotzee <jaco@doodlesdigital.co.za>
2017-12-04 23:42:38 +0200

    added data to array

--- js/quote.js
+++ js/quote.js
@@ -1,2 +1,19 @@
-// Create an array of JavaScript objects to hold the data for your quotes. Name the array quotes. The quotes array should be accessible in the global scope.
-var quotes = [];
+// An array of JavaScript objects to hold the data for the quotes.
+var quotes = [
+    {
+        quote: 'Education without values, as useful as it is, seems rather to make man a more clever devil.',
+        source: 'C.S. Lewis',
+        citation: 'www.brainyquote.com'
+    },
+    {
+        quote: 'Be a yardstick of quality. Some people aren\'t used to an environment where excellence is expected.',
+        source: 'Steve Jobs',
+        citation: 'www.brainyquote.com'        
+    },
+    {
+        quote: 'Do not be anxious about tomorrow, for tomorrow will be anxious for itself. Let the day\'s own trouble be sufficient for the day.',
+        source: 'Jesus Christ',
+        citation: 'The Holy Bible'        
+    }
+];
+
