bd07aee3d71dc97e83cd59229526b5f178dc741c
Jaco Kotzee <jaco@doodlesdigital.co.za>
2017-12-05 00:25:39 +0200

    created functions for getRandomQuote and printQuote

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,34 @@
-// Create a function named getRandomQuote which:
-    // selects a random quote object from the quotes array
-    // returns the randomly selected quote object
+// selects a random quote object from the quotes array & returns the randomly selected quote object
+function getRandomQuote() {
+    // gets a random number that's between 0 and the amount of objects (quotes) in the quotes array
+    var randomNumber = Math.floor( Math.random() * quotes.length );
+    return quotes[randomNumber];
+}
+
+function printQuote() {
+    // printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+    var quoteObject = getRandomQuote();
+    var html;
+    // printQuote constructs a string containing the different properties of the quote object
+    html = '<p class="quote">' + quoteObject.quote + '</p>';
+    html += '<p class="source">' + quoteObject.source;
+    if (quoteObject.citation) {
+        html += '<span class="citation">' + quoteObject.citation + '</span>';
+    }
+    if (quoteObject.year) {
+        html += '<span class="citation">' + quoteObject.year + '</span>';
+    }
+    html += '</p>';
+    var print = document.getElementById('quote-box').innerHTML = html;
+    return print;
+}
+
+
+
+
+
+
+// using the following HTML template:
+
+
+console.log(getRandomQuote());
--- js/quote.js
+++ js/quote.js
@@ -8,12 +8,17 @@ var quotes = [
     {
         quote: 'Be a yardstick of quality. Some people aren\'t used to an environment where excellence is expected.',
         source: 'Steve Jobs',
-        citation: 'www.brainyquote.com'        
+        citation: 'www.brainyquote.com',
+        year: 1997        
     },
     {
         quote: 'Do not be anxious about tomorrow, for tomorrow will be anxious for itself. Let the day\'s own trouble be sufficient for the day.',
         source: 'Jesus Christ',
         citation: 'The Holy Bible'        
+    },
+    {
+        quote: 'Human behavior flows from three main sources: desire, emotion, and knowledge.',
+        source: 'Plato'       
     }
 ];
 
